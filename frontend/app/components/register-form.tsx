574396b9b1ceb87a13c53a366b503b3c5cd9c5b3
Mosab Mohamed <mosab.f.r@gmail.com>
2025-06-16 14:05:08 +0300

    [frontend] add login redirct link in register

--- frontend/app/components/register-form.tsx
+++ frontend/app/components/register-form.tsx
@@ -56,6 +56,12 @@ export function RegisterForm({
                 </Button>
               </div>
             </div>
+            <div className="mt-4 text-center text-sm">
+              Already have an account?{" "}
+              <a href="/login" className="underline underline-offset-4">
+                Login
+              </a>
+            </div>
           </form>
         </CardContent>
       </Card>
