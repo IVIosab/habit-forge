b6ff1aeb998632cb24f2e635a39538bcee6cee56
Mosab Mohamed <mosab.f.r@gmail.com>
2025-06-18 20:02:31 +0300

    [frontend add initial design for sidebar]

---
+++ frontend/src/components/app-sidebar.tsx
@@ -0,0 +1,50 @@
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarGroup,
+	SidebarGroupContent,
+	SidebarGroupLabel,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+// Menu items.
+const items = [
+	{
+		title: "Habits",
+		url: "/habits",
+	},
+	{
+		title: "Statistics",
+		url: "/statistics",
+	},
+];
+
+export function AppSidebar() {
+	return (
+		<Sidebar>
+			<SidebarContent>
+				<SidebarGroup>
+					<SidebarGroupContent>
+						<SidebarMenu>
+							{items.map((item) => (
+								<SidebarMenuItem key={item.title}>
+									<SidebarMenuButton asChild>
+										<a href={item.url}>
+											<span>{item.title}</span>
+										</a>
+									</SidebarMenuButton>
+								</SidebarMenuItem>
+							))}
+						</SidebarMenu>
+					</SidebarGroupContent>
+				</SidebarGroup>
+			</SidebarContent>
+			{/* <SidebarFooter>
+                //TODO Add user card footer in sidebar.
+            </SidebarFooter> */}
+		</Sidebar>
+	);
+}
