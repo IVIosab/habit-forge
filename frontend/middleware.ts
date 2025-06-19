f7d00d3475baf095c8ddff3c1ca16480e5bb8899
Mosab Mohamed <mosab.f.r@gmail.com>
2025-06-19 22:26:18 +0300

    [frontend] add middleware for auth redirection

--- frontend/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-
-export function middleware(request: NextRequest) {
-	//implement some way of checking if the user is logged in and redirecting them to the login page if they are not
-
-	return NextResponse.next();
-}
---
+++ frontend/src/middleware.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import { getSessionCookie } from "better-auth/cookies";
+
+export async function middleware(request: Request) {
+	const sessionCookie = getSessionCookie(request);
+	const url = new URL(request.url);
+	if (
+		!sessionCookie &&
+		!["/", "/login", "/register"].includes(url.pathname)
+	) {
+		return NextResponse.redirect(new URL("/", request.url));
+	}
+
+	if (sessionCookie && ["/", "/login", "/register"].includes(url.pathname)) {
+		return NextResponse.redirect(new URL("/habits", request.url));
+	}
+
+	return NextResponse.next();
+}
+
+export const config = {
+	matcher: [
+		"/((?!api|_next/static|_next/image|favicon.ico|.*\\.png$|.*\\.css$).*)",
+	],
+};
