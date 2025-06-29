1b505e48c0ea995d09e4bbd76b0b9f3be4872265
Mosab Mohamed <mosab.f.r@gmail.com>
2025-06-29 03:14:16 +0300

    [backend] add openapi spec and postman collection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,35 +1,35 @@
-import express, { Request, Response } from "express";
-import router from "./routes/index.js";
-import { logger } from "./middlewares/logging.middleware.js";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import { auth } from "./utils/auth.js";
-import { toNodeHandler } from "better-auth/node";
-import "dotenv/config";
+import express, { Request, Response } from "express"
+import router from "./routes/index.js"
+import { logger } from "./middlewares/logging.middleware.js"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import { auth } from "./utils/auth.js"
+import { toNodeHandler } from "better-auth/node"
+import "dotenv/config"
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = express()
+const PORT = process.env.PORT || 3000
 app.use(
   cors({
     origin: process.env.FRONTEND_URL || "http://localhost:3001",
-    credentials: true,
+    credentials: true
   })
-);
-app.use(cookieParser());
-app.use(logger);
+)
+app.use(cookieParser())
+app.use(logger)
 
-app.all("/api/auth/{*any}", toNodeHandler(auth));
+app.all("/api/auth/{*any}", toNodeHandler(auth))
 
 //use these after the auth middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-app.use("/api", router);
+app.use("/api", router)
 
 app.get("/api", (req: Request, res: Response) => {
-  res.send("Welcome to LinkNest API!");
-});
+  res.send("Welcome to HabitForge API!")
+})
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+  console.log(`🚀 Server running on http://localhost:${PORT}`)
+})
